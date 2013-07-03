6ef83db8cdfab1d3b80fb8f7da7946140d456e9a
Harald Kirschner <hkirschner@mozilla.com>
2013-07-02 19:45:45 -0500

    Reduced test case and added remote redirect server using a small node.js app

---
+++ redirects/redirects/script.js
@@ -0,0 +1,1 @@
+alert('SUCCESS! ' + location.hash);
--- redirects/source.js
+++ redirects/source.js
@@ -1,10 +1,11 @@
 window.onload = function () {
     document.getElementById("open-with-redirection").onclick = function () {
-        window.open("http://MichaelPro.local/~mbishop/index.html");
+    	var redirector = 'http://fathomless-retreat-3837.herokuapp.com'
+    	var url = 'http://app-1.digitarald.com/auth';
+			window.open(redirector + '/?' + encodeURIComponent(url));
     };
 
     document.getElementById("open-without-redirection").onclick = function () {
-    window.open("/redirects/authenticated.html");
+			window.open("/redirects/authenticated.html");
     };
 }
-
